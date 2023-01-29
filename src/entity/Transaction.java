1607c2a6a863116eeb62617881f5f81f9b269939
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-01-29 20:49:13 -0300

    add Transaction entity

---
+++ src/entity/Transaction.java
@@ -0,0 +1,38 @@
+package entity;
+
+import entity.Account;
+import java.time.LocalDate;
+
+public abstract class Transaction {
+  private double value;
+  private Account account;
+  private LocaleDate createdAt;
+  private String channel;
+
+  public Transaction (
+    double value,
+    Account account,
+    String channel
+  ) {
+    this.value = value;
+    this.account = account;
+    this.channel = channel;
+    this.createdAt = LocalDate.now();
+  }
+
+  public double getValue() {
+    return this.value;
+  }
+
+  public Account getAccount() {
+    return this.account;
+  }
+
+  public String getCreatedAt() {
+    return this.createdAt;
+  }
+
+  public String getChannel() {
+    return this.channel;
+  }
+}
