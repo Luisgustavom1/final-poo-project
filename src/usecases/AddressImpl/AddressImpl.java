d2a8e01bd485f3b221cd7fdc78e7f66adbbcd708
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-01-29 18:36:37 -0300

    add address class

---
+++ src/usecases/AddressImpl/AddressImpl.java
@@ -0,0 +1,35 @@
+package usecases;
+
+import entity.Address;
+
+public class AddressImpl extends Address {
+  public AddressImpl(
+    String city,
+    String state,
+    String country,
+    String neighborhood,
+    String streetName,
+    String streetNumber
+  ) {
+    super(
+      city,
+      state,
+      country,
+      neighborhood,
+      streetName,
+      streetNumber
+    );
+  }
+
+  public String toString() {
+    return "Address{" +
+        "City='" + this.getCity() + '\'' +
+        ", State='" + this.getState() + '\'' +
+        ", Country='" + this.getCountry() + '\'' +
+        ", Neighborhood='" + this.getNeighborhood() + '\'' +
+        ", StreetName=" + this.getStreetName() +
+        ", StreetNumber=" + this.getStreetNumber() +
+        '}';
+  }
+}
+
