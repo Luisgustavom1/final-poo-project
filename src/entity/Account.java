37fc3b46ec601f582dd2feb65ffd4ee5cced19c2
luisgustavom1 <luisgustavomacedo13@gmail.com>
2023-01-31 21:49:48 -0300

    add to account toString()

--- src/entity/Account.java
+++ src/entity/Account.java
@@ -18,13 +18,22 @@ public abstract class Account implements Serializable {
         this.accNumber = accNumber;
         this.balance = balance;
         this.createdAt = createdAt;
-        this.lastTransactionAt = null;
+        this.lastTransactionAt = LocalDate.now();
         this.agency = agency;
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        String lastTransactionAtString = this.lastTransactionAt == null ? "Not found transactions" : this.lastTransactionAt.toString();
+
+        return "Account { " +
+            "AccNumber = '" + this.accNumber + '\'' +
+            ", Balance = '" + this.balance + '\'' +
+            ", Created at = '" + this.createdAt + '\'' +
+            ", Last transaction at = '" + lastTransactionAtString + '\'' +
+            ", Agency name = '" + this.agency.getName() + '\'' +
+            ", Agency number = '" + this.agency.getNumber() + '\'' +
+            '}';
     }
 
     public boolean isActive() {
